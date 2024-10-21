846e9b9019e7b8d0b8c98c4c0517a6fd505b809b
TVSCSK <108569220+peninsula101@users.noreply.github.com>
2024-10-21 13:23:47 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import './App.css'
 import HistoryPage from './components/HistoryPage'
+import './App.css'
 
 // These are the list used in the application. You can move them to any component needed.
 
